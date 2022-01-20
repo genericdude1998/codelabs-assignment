e063df8134d5cd3fe831c237ed79e0c15471e27a
vsenzo <vsenzo@hello.com>
2022-01-20 15:15:51 +0000

    added home route

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,24 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Survey from './surveysList/survey/Survey';
 import SurveyCreator from './surveyCreator/SurveyCreator';
 import SurveysList from './surveysList/SurveysList';
+import Home from './home/Home';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={
+                    <Home/>
+                } />
                 <Route path='/newSurvey' element={
                     <SurveyCreator />
-                }>
-                </Route>
+                } />
                 <Route path='/surveysList' element={
                     <SurveysList />
-                }>
-                </Route>
+                } />
                 <Route path='/takeSurvey' element={
                     <Survey title={'title'}/>
-                }>
-                </Route>
+                } />
             </Routes>
         </BrowserRouter>
     );
