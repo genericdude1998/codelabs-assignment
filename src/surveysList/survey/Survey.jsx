f55011c02b7b9a3e384d3f9f629aea2c8c8d1bc4
vsenzo <vsenzo@hello.com>
2022-01-20 15:24:44 +0000

    added link to take survey in surveys list

--- src/surveysList/survey/Survey.jsx
+++ src/surveysList/survey/Survey.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Survey = ({title, surveyId}) => {
 
     return (
         <div>
             <h1>{title}</h1>
+            <Link to={`/takeSurvey/:${title}`}>Take this Survey</Link>
         </div>
     );
 }
