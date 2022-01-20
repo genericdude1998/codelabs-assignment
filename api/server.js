449d4f01369241b71f02fb757f450fe4802f7995
vsenzo <vsenzo@hello.com>
2022-01-20 15:35:08 +0000

    added route to get survey based on title

--- api/server.js
+++ api/server.js
@@ -18,6 +18,15 @@ const devServer = (devServer) => {
         console.log(surveys);
         res.json(surveys);
     });
+
+    app.get('/getSurveys/:title', (req,res) => {
+        console.log(req.params.title);
+        if(surveys.filter(survey => survey.title === req.params.title)){
+            console.log(surveys.filter(survey => survey.title === req.params.title))
+            res.json(surveys.filter(survey => survey.title === req.params.title));
+        }
+        res.json(surveys);
+    });
 }
 
 module.exports = devServer;
--- src/takeSurvey/SurveyDisplayer.jsx
+++ src/takeSurvey/SurveyDisplayer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Surveydisplayer = () => {
+    
     return (
         <div>
             
