ee384bd971690b66bbc65e20438679ad82807f51
vsenzo <vsenzo@hello.com>
2022-01-22 11:06:37 +0000

    added css to survey displayer

--- src/takeSurvey/SurveyDisplayer.jsx
+++ src/takeSurvey/SurveyDisplayer.jsx
@@ -60,38 +60,49 @@ const SurveyDisplayer = () => {
         return (
         <div>
             <Navbar />
-            <h1>{survey.title}</h1>
-            <p>{survey.questions[currentQuestionId].name}</p>
-            <form >
-                <label htmlFor='Yes'>Yes</label>
-                <input 
-                    type="radio" 
-                    id="Yes" 
-                    name='Answer' 
-                    onChange={onChangeAnswer} 
-                    value={'Yes'}
-                    checked={answer === 'Yes'}
-                />
+            <h1 className='text-center p-2'>{survey.title}</h1>
+            <form className='flex flex-col p-2' className="
+                        bg-slate-200
+                        m-5
+                        border-t-4 border-indigo-500
+                        break-all">
+                <p className='text-center m-5'>{survey.questions[currentQuestionId].name}</p>
+                <div className='flex justify-around m-10'>
+                    <div>
+                        <label htmlFor='Yes'>Yes</label>
+                        <input className='self-center'
+                            type="radio" 
+                            id="Yes" 
+                            name='Answer' 
+                            onChange={onChangeAnswer} 
+                            value={'Yes'}
+                            checked={answer === 'Yes'}
+                        />
+                    </div>
 
-                <label htmlFor='No'>No</label>
-                <input 
-                    type="radio" 
-                    id="No" 
-                    name='Answer' 
-                    onChange={onChangeAnswer} 
-                    value={'No'}
-                    checked={answer === 'No'}
-                />
-
-                {currentQuestionId !== 0 ? 
-                    <button onClick={onPrevious}>Previous</button> 
-                        : 
-                    null}
-                {currentQuestionId !== survey.questions.length - 1 ? 
-                    <button onClick={onNext}>Next</button>
-                        : 
-                    <button onClick={onFinal}>Send</button>
-                } 
+                    <div>
+                        <label htmlFor='No'>No</label>
+                        <input className='self-center'
+                            type="radio" 
+                            id="No" 
+                            name='Answer' 
+                            onChange={onChangeAnswer} 
+                            value={'No'}
+                            checked={answer === 'No'}
+                        />
+                    </div>
+                </div>
+                <div>
+                    {currentQuestionId !== 0 ? 
+                        <button className="m-1 button" onClick={onPrevious}>Previous</button> 
+                            : 
+                        null}
+                    {currentQuestionId !== survey.questions.length - 1 ? 
+                        <button className="m-1 button" onClick={onNext}>Next</button>
+                            : 
+                        <button className="m-1 button" onClick={onFinal}>Send</button>
+                    } 
+                </div>
             </form>
             <h5>{error ? error: null}</h5>
             <p>{`Question ${currentQuestionId + 1} of ${survey.questions.length}`}</p>
