5bfbb4a3ec580d597858abff64c9afbf3156ed3a
vsenzo <vsenzo@hello.com>
2022-01-22 12:43:05 +0000

    added responsive navbar

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -7,9 +7,15 @@ const Navbar = () => {
                 bg-indigo-500
                 h-10
             ">
-                <Link to='/' className="text-white h-full navBarLink m-5">Home</Link>
-                <Link to='/newSurvey' className="text-white h-full navBarLink m-5">Create Survey</Link>      
-                <Link to='/surveysList' className="text-white h-full navBarLink m-5">My Surveys</Link>  
+                <Link to='/' className="text-white h-full text-xs m-1 navBarLink sm:text-base
+                    sm:m-2
+                ">Home</Link>
+                <Link to='/newSurvey' className="text-white h-full text-xs m-1 navBarLink sm:text-base
+                    sm:m-2
+                ">Create Survey</Link>      
+                <Link to='/surveysList' className="text-white h-full text-xs m-1 navBarLink sm:text-base
+                    sm:m-2
+                ">My Surveys</Link>  
         </div >
     );
 }
