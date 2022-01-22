a3188649933c59bd987d44cc9826c90493e72329
vsenzo <vsenzo@hello.com>
2022-01-22 12:49:45 +0000

    finalied navbar

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -5,16 +5,12 @@ const Navbar = () => {
     return (
         <div className="
                 bg-indigo-500
-                h-10
             ">
-                <Link to='/' className="text-white h-full text-xs m-1 navBarLink sm:text-base
-                    sm:m-2
+                <Link to='/' className="text-white h-full text-xs navBarLink 
                 ">Home</Link>
-                <Link to='/newSurvey' className="text-white h-full text-xs m-1 navBarLink sm:text-base
-                    sm:m-2
+                <Link to='/newSurvey' className="text-white h-full text-xs ml-1 navBarLink
                 ">Create Survey</Link>      
-                <Link to='/surveysList' className="text-white h-full text-xs m-1 navBarLink sm:text-base
-                    sm:m-2
+                <Link to='/surveysList' className="text-white h-full text-xs ml-1 navBarLink
                 ">My Surveys</Link>  
         </div >
     );
